4e139bd96b52baa4c8e3940d3cf48d528bdeaec3
brulibra <brulibra03@hotmail.com>
2022-05-25 12:08:24 -0300

    arrumando identação login page

--- src/pages/loginPage/login.jsx
+++ src/pages/loginPage/login.jsx
@@ -13,27 +13,29 @@ function Login() {
             <div className="logo-image">
                 <img src={logoBurguerTiaNena} alt="logo"></img>
             </div>
-            <div className="assign-role">
-                <label> <RadioButton /> Garçom </label>
-                <label><RadioButton /> Cozinha </label>
-            </div>
             <form className="form-login">
-                <label> Email:<br />
-                    <InputTxt
-                        type="email"
-                        name="email"
-                        placeholder="email@exemplo.com"
-                        required={true}
-                    />
-                </label><br />
-                <label> Senha:<br />
-                    <InputTxt
-                        type="password"
-                        name="password"
-                        placeholder="Digite uma senha de até 8 caracteres"
-                        required={true}
-                    />
-                </label>
+                <section className="assign-role">
+                    <label> <RadioButton /> Garçom </label>
+                    <label><RadioButton /> Cozinha </label>
+                </section>
+                <section className="login-information">
+                    <label> Email:<br />
+                        <InputTxt
+                            type="email"
+                            name="email"
+                            placeholder="email@exemplo.com"
+                            required={true}
+                        />
+                    </label><br />
+                    <label> Senha:<br />
+                        <InputTxt
+                            type="password"
+                            name="password"
+                            placeholder="Digite uma senha de até 8 caracteres"
+                            required={true}
+                        />
+                    </label>
+                </section>
             </form>
             <div className="onClick-button">
                 <ClickButon onClick="Entrar" />
