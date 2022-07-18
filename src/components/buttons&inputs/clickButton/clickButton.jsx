638d206caa4516fc0a619fa1b2c15ebd790ba5e9
brulibra <brulibra03@hotmail.com>
2022-07-18 12:35:39 -0300

    criação card produtos - finalizar

--- src/components/buttons&inputs/clickButton/clickButton.jsx
+++ src/components/buttons&inputs/clickButton/clickButton.jsx
@@ -1,16 +1,16 @@
 import "./clickButton.css"
 import "../../../globalStyle/colors.css"
 
-function ClickButon({type, onClick, txtBtn}) {
-    return (
-        <button
-            className="general-orange-button"
-            type={type}
-            onClick={onClick}
-        >
-            {txtBtn}
-        </button>
-    )
+function ClickButon({ type, onClick, txtBtn }) {
+	return (
+		<button
+			className="general-orange-button"
+			type={type}
+			onClick={onClick}
+		>
+			{txtBtn}
+		</button>
+	)
 }
 
 export default ClickButon
--- src/components/productCard/productCard.jsx
+++ src/components/productCard/productCard.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
-function Product () {
-    return(
-        <>
-        
-        </>
-    )
+function Product({ image, name, price }) {
+	return (
+		<li className="productCard">
+			<button>
+				<picture>
+					<img src={image} alt="productImage" className="productImg" />
+				</picture>
+				<section className="productInfo">
+					<h3>{name}</h3>
+					<p>Preço R${price}</p>
+				</section>
+			</button>
+		</li>
+	)
 }
 export default Product
